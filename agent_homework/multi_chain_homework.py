c54a7137a4ccaf94fc4af5513de660235a9e6163
shengj8329 <shengjie8329@163.com>
2024-09-14 18:05:38 +0800

    test o1 model

--- agent_homework/multi_chain_homework.py
+++ agent_homework/multi_chain_homework.py
@@ -15,7 +15,7 @@ java_chain = (
         ChatPromptTemplate.from_template(
             "请用java 语言实现下面的功能需求，给出示例代码: \n 需求: {query}"
         )
-        | ChatOpenAI(openai_api_base="https://ai-yyds.com/v1", model="gpt-4o-mini")
+        | ChatOpenAI(openai_api_base="https://ai-yyds.com/v1", model="o1-mini", temperature=1 )
         | StrOutputParser()
 )
 
@@ -23,7 +23,7 @@ python_chain = (
         ChatPromptTemplate.from_template(
             "请用python 语言实现下面的功能需求，给出示例代码: \n 需求: {query}"
         )
-        | ChatOpenAI(openai_api_base="https://ai-yyds.com/v1", model="gpt-4o-mini")
+        | ChatOpenAI(openai_api_base="https://ai-yyds.com/v1", model="o1-mini", temperature=1)
         | StrOutputParser()
 )
 
@@ -38,7 +38,7 @@ process_chain = {
                     "python_code": python_chain
                 } | RunnableLambda(response_process)
 
-rs = process_chain.invoke({"query": "请写一个快速排序的例子"})
+rs = process_chain.invoke({"query": "请写出一个快速排序的代码"})
 # print(str(rs))
 print(f"java:  \n {rs['java_code']}\n")
 print(f"python:  \n {rs['python_code']}\n")
